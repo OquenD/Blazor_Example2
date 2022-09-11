78b59da32d0749ddde6c704d384e4dd7323d65f2
OquenD <oquendodavid45@gmail.com>
2022-09-10 22:46:20 -0500

    Almost

--- Punto2/Classes/Pastel.cs
+++ Punto2/Classes/Pastel.cs
@@ -6,51 +6,54 @@ public class Pastel : IPastelInterface{
 
     public string nombre="";
     public int size=0;
+    private float total =0;
 
     //Inicializamos la lista de ingredientes
     List<Ingrediente> ListaIngredientes = new List<Ingrediente>();
 
     
     public float CalcularCosto(){
-
-
-    return 1;
-
+        foreach (Ingrediente ingrediente in ListaIngredientes){
+                 total= total + ingrediente.price;
+        }
+    return total;
    }
+
+   
     //Recorrer la lista, cada item es una instancia de la clase Ingrediente
     public void ListarIngredientes(){
        for(int i= 0; i < ListaIngredientes.Count; i++){
-        Console.WriteLine(ListaIngredientes[i]);
+            Console.WriteLine(ListaIngredientes[i]);
         }
 
         foreach (Ingrediente ingrediente in ListaIngredientes){
-        Console.WriteLine(ingrediente);
+            Console.WriteLine(ingrediente);
         }
     }
 
     //Devuelve cuantos items tiene la lista
     public int CantidadIngredientes(){
-
         int count = ListaIngredientes.Count;
         return count;
-
-
     }
 
 
     //Agregando un ingrediente a la lista
     public void AgregarIngrediente(string nombre, int cantidad, float precio){
-
-        ListaIngredientes.Add(new Ingrediente( nombre,  cantidad, precio));
-
+        ListaIngredientes.Add(new Ingrediente( nombre.ToUpper(),  cantidad, precio));
     }
 
     //Eliminar elemento
-     public void ElimarIngrediente(int pos){
-
-    ListaIngredientes.RemoveAt(pos);
+     public void ElimarIngrediente(string nombre){
+        int pos=0;
+          for(int i= 0; i < ListaIngredientes.Count; i++){
+            if (ListaIngredientes[i].name==nombre.ToUpper()){
+                pos = i;
+                ListaIngredientes.RemoveAt(pos);
+                break;
+            }
         
-
+        }
     }
 
 }
--- Punto2/Interface/IPastelInterface.cs
+++ Punto2/Interface/IPastelInterface.cs
@@ -6,7 +6,7 @@ public float CalcularCosto();
 public void ListarIngredientes();
 public int CantidadIngredientes(); 
 public void AgregarIngrediente(string nombre, int cantidad, float precio);
-public void ElimarIngrediente(int pos);
+public void ElimarIngrediente(string nombre);
 
 }
 
