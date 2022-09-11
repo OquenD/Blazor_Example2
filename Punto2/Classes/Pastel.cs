9b7e2c71f7b58a77c2a420867eb4467ec71d3fd6
OquenD <oquendodavid45@gmail.com>
2022-09-11 14:19:50 -0500

    Done

--- Punto2/Classes/Pastel.cs
+++ Punto2/Classes/Pastel.cs
@@ -5,34 +5,55 @@ using System.Collections.Generic;
 public class Pastel : IPastelInterface{
 
     public string nombre="";
-    public int size=0;
     private float total =0;
+    public bool listaVacia=true;
 
     //Inicializamos la lista de ingredientes
     List<Ingrediente> ListaIngredientes = new List<Ingrediente>();
 
+
+ public bool ListaVacio(){
+        if(ListaIngredientes.Count !=0)
+        {
+            return false;
+        }
+        return true;
+
+    }
     
     public float CalcularCosto(){
-        foreach (Ingrediente ingrediente in ListaIngredientes){
-                 total= total + ingrediente.price;
+         if(ListaVacio())
+        {
+            Console.WriteLine("No se puede calcular sin Ingredientes \n");
+            return 0;
+            
         }
-    return total;
+        total=0;
+        foreach (Ingrediente ingrediente in ListaIngredientes){
+                      total= total + ingrediente.price;
+                    }   
+        return total;
    }
 
    
     //Recorrer la lista, cada item es una instancia de la clase Ingrediente
     public void ListarIngredientes(){
-       for(int i= 0; i < ListaIngredientes.Count; i++){
-            Console.WriteLine(ListaIngredientes[i]);
+        if(ListaVacio())
+        {
+            Console.WriteLine("No hay Ingredientes para listar \n");
         }
-
         foreach (Ingrediente ingrediente in ListaIngredientes){
-            Console.WriteLine(ingrediente);
-        }
+            Console.WriteLine("-Ingrediente: "+ingrediente.name +" -Cantidad: "+ingrediente.cantidadIngrediente+" -Precio: "+ ingrediente.price+"\n");
+            }
+
     }
 
     //Devuelve cuantos items tiene la lista
     public int CantidadIngredientes(){
+        if(ListaVacio()){
+            Console.WriteLine("No hay Ingredientes \n");
+            return 0;
+        }
         int count = ListaIngredientes.Count;
         return count;
     }
@@ -41,19 +62,26 @@ public class Pastel : IPastelInterface{
     //Agregando un ingrediente a la lista
     public void AgregarIngrediente(string nombre, int cantidad, float precio){
         ListaIngredientes.Add(new Ingrediente( nombre.ToUpper(),  cantidad, precio));
+        Console.WriteLine("Ingrediente ingresado \n ");
+
     }
 
     //Eliminar elemento
-     public void ElimarIngrediente(string nombre){
+    public void ElimarIngrediente(string nombre){
+        if(ListaVacio()){
+            Console.WriteLine("No hay Ingredientes para eliminar \n");
+        }   
         int pos=0;
-          for(int i= 0; i < ListaIngredientes.Count; i++){
+        for(int i= 0; i < ListaIngredientes.Count; i++){
             if (ListaIngredientes[i].name==nombre.ToUpper()){
                 pos = i;
                 ListaIngredientes.RemoveAt(pos);
+                Console.WriteLine("Ingrediente eliminado \n");
+
                 break;
+                }
+                
             }
-        
-        }
     }
 
 }
--- Punto2/Program.cs
+++ Punto2/Program.cs
@@ -1,4 +1,63 @@
 ﻿using ENTREGABLE2.Classes;
 
+bool condicion = true;
+string nameIngrediente;
+int cantIngrediente;
+float precioIngrediente;
 
+Pastel pastel = new Pastel();
+
+while (condicion)
+{
+    Console.WriteLine("\nIngrese el nombre del Pastel (1)"); 
+    Console.WriteLine("Ingrese un ingrediente (2)"); 
+    Console.WriteLine("Listar Ingredientes (3)");
+    Console.WriteLine("Calcular Cantidad de Ingredientes(4)");
+    Console.WriteLine("Calcular Costo(5)");
+    Console.WriteLine("Exit (0)");
+
+    int menu = int.Parse(Console.ReadLine());
+
+    switch (menu)
+    {
+        case 1:
+            Console.WriteLine("\nNOMBRE: ");      
+            pastel.nombre = (Console.ReadLine()).ToUpper();
+            break;
+
+        case 2:
+        
+            Console.WriteLine("\nIngrese el nombre del ingrediente"); 
+            nameIngrediente= (Console.ReadLine()).ToUpper();
+            Console.WriteLine("\nIngrese la Cantidad"); 
+            cantIngrediente = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nIngrese el precio del ingrediente"); 
+            precioIngrediente= float.Parse(Console.ReadLine());
+            pastel.AgregarIngrediente(nameIngrediente,cantIngrediente,precioIngrediente);
+            break; 
+
+        case 3:
+            Console.WriteLine($"\n{pastel.nombre} ");     
+            Console.WriteLine("INGREDIENTES: ");         
+            pastel.ListarIngredientes();
+            break; 
+
+        case 4:
+            Console.WriteLine($"\n{pastel.nombre} ");     
+            Console.WriteLine("CANTIDAD DE INGREDIENTES: "); 
+            Console.WriteLine(pastel.CantidadIngredientes());
+            break; 
+
+        case 5:
+            Console.WriteLine($"\n{pastel.nombre} ");     
+            Console.WriteLine("COSTO DEL PASTEL: "); 
+            Console.WriteLine(pastel.CalcularCosto()); 
+            break; 
+
+        case 0:
+        default: 
+            condicion = false;
+            break;
+    }
+}
 
